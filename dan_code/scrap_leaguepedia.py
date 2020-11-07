e57ceacde255582c99248da667127f0bed19aff5
drigobon <drigobon@gmail.com>
2020-11-06 21:39:42 -0500

    Update scrap_leaguepedia.py

--- dan_code/scrap_leaguepedia.py
+++ dan_code/scrap_leaguepedia.py
@@ -1,13 +1,37 @@
+import numpy as np
+
 from utils import *
 import models
 
-if __name__ == "__main__":
-	# scrap every row
-	url = construct_url_for_team("Team_SoloMid")
 
+if __name__ == "__main__":
+
+	teams = ['DAMWON_Gaming', 'DRX', 'FlyQuest', 'Fnatic', 'G2_Esports', 'Gen.G', 'JD_Gaming', 'LGD_Gaming', 'Machi_Esports', 'PSG_Talon', 'Rogue_(European_Team)', 'Suning', 'Team_Liquid', 'Team_SoloMid', 'Top_Esports', 'Unicorns_Of_Love.CIS', ]
+
+#	for team in teams:
+
+	team = 'Team_SoloMid'
+
+	url = construct_url_for_team(team)
 	table_rows = extract_table_rows_from_url(url)
 
+	all_players = set()
+	all_games = list()
+
 	for row in table_rows:
-		extract_game_from_row(row, True)
+		game = extract_game_from_row(row, True)
+
+		all_games.append(game)
+		all_players = set(list(all_players) + game.players)
+
+	print(all_players)
+
+
+
+
+
+	# Given data, run analysis
+
+
 
 
